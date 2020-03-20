49dedd264c7db25af6222b1bea559b0346ac666a
Mr.Flava <thatelitemaili33t@gmail.com>
2020-03-20 03:03:49 +0200

    Added scrollup feature

--- js/main.js
+++ js/main.js
@@ -61,5 +61,23 @@ $(function() {
   }
   
 });
+jQuery(document).ready(function ($) {
+
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 600) {
+            $('.scrollup').fadeIn('slow');
+        } else {
+            $('.scrollup').fadeOut('slow');
+        }
+    });
+
+    $('.scrollup').click(function () {
+
+        $("html, body").animate({scrollTop: 0}, 1000);
+
+        return false;
+    });
+
+});
 
 
